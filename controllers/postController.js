7c443580957baa51747d15fa1bbaae00bc3a772a
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-09-25 17:42:06 -0400

    initial commit

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,20 @@
+let Post = require('../models/Post');
 exports.viewCreateScreen = function(req, res){
     res.render('create-post');
+}
+exports.create = function(req, res){
+    let post = new Post(req.body, req.session._id);
+    post.create().then(function(){
+        res.send('Post has been Created');
+    }).catch(function(error){
+        res.send(error);
+    }); 
+}
+exports.viewSingle = async function(req, res){
+    try{
+        let post = await Post.findSingleById(req.params.id);
+        res.render('post-single-screen', {post: post});
+    }catch{
+        res.send('This is 404 template');
+    }
 }
---
+++ models/Post.js
@@ -0,0 +1,58 @@
+const postCollection = require('../db').db().collection('posts');
+const ObjectID = require('mongodb').ObjectID;
+
+let Post = function(data, userid){
+    this.data = data;
+    this.errors = [];
+    this.userid = userid;
+}
+Post.prototype.cleanUp = function(){
+    if(typeof (this.data.title) != 'string'){ this.data.title = ""}
+    if(typeof (this.data.body) != 'string'){ this.data.body = ""}
+
+    this.data = {
+        title: this.data.title.trim(),
+        body: this.data.body.trim(),
+        author: ObjectID(this.userid),
+        createdDate: new Date()
+    }
+}
+
+Post.prototype.validate = function(){
+    if(this.data.title == ""){this.errors.push('You must provide a title')}
+    if(this.data.body == ""){this.errors.push('You must provide a post content')}
+}
+
+Post.prototype.create = function(){
+    return new Promise((resolve, reject) => {
+        this.cleanUp();
+        this.validate();
+        if(!this.errors.length){
+            postCollection.insertOne(this.data).then(()=>{
+                resolve();
+            }).catch(()=>{
+                this.errors.push('Please try it again');
+                reject(this.errors);
+            });
+        }else{
+            reject(this.errors);
+        }
+    });
+}
+
+Post.findSingleById = function(id){
+    return new Promise(async function(resolve, reject){
+        if(typeof(id) != 'string' || !ObjectID.isValid(id)){
+            reject()
+            return 
+        }
+        let post = await postCollection.findOne({_id: new ObjectID(id)});
+        if(post){
+            resolve(post);
+        }else{
+            reject();
+        }
+    });
+}
+
+module.exports = Post;
