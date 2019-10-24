162e5768aeb3a0db9741e88cc38af19f7e54e6a8
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-10-24 17:13:30 -0400

    NodeJS Complex App

--- db.js
+++ db.js
@@ -6,5 +6,5 @@ const mongodb = require('mongodb');
 mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
     module.exports = client;
     const app = require('./app');
-    app.listen(process.env.PORT);
+    app.listen(process.env.PORT || 5000);
 });
